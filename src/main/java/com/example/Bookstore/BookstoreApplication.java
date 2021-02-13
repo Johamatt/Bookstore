52e4b996975a11e8d4215b833e8afb8ddba56a4b
johamatt94 <johamatt94@gmail.com>
2021-02-13 20:04:30 +0200

    c3.1

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,5 +14,18 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repo)  {
+		return (args) -> {
+			Book b = new Book("The Lord of the Rings", "J.R.R. Tolkien", 2012, "1782808086", 16.99);
+			Book b1 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1998, "9781782808084", 25.90);
+			
+			repo.save(b);
+			repo.save(b1);
+		};
+	}
 
 }
+
+
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,17 +1,33 @@
 package com.example.Bookstore.domain;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Getter
 @Setter
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long Id;
+	
+	public Book(String title, String author, int year, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
 	
 	private String title;
 	private String author;
